d7bbf683d4f18ab0bcb523956a8551a6c31a1804
parappa20 <jamesaaronlindsey182@gmail.com>
2022-03-11 14:07:14 -0600

    update styling for posts

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import Avatar from '@mui/material/Avatar';
+import './style.css';
 
 const PostList = ({ posts, title }) => {
     if (!posts.length) {
@@ -6,22 +8,23 @@ const PostList = ({ posts, title }) => {
     }
 
     return (
-        <div>
+        <div className='post'>
             <h3>{title}</h3>
             {posts &&
               posts.map(post => (
                   <div key={post._id} >
-                      <p className='card-header'>
+                      <p className='post__header'>
                           <Link
                             to={`/profile/${post.username}`}
                             style={{ fontWeight: 700 }}
                             className='text-light'
+                            style={{ textDecoration: 'none' }}
                           >
                               {post.username}
                           </Link>{' '}
                           post on {post.createdAt}
                       </p>
-                      <div className='card-body'>
+                      <div className='post__body'>
                           <Link to={`/post/${post._id}`}>
                               <p>{post.postText}</p>
                               <p className='mb-0'>
