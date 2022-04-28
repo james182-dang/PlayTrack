95ba94f431a284b1a0d745613797812b2a98b9b2
parappa20 <jamesaaronlindsey182@gmail.com>
2022-04-28 17:51:58 -0500

    server.js typo fix

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-if (process.env.NODE_END === 'production') {
+if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
